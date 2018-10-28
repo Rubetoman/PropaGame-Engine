86561e39ad696615196625f83fa78691d1bf1b69
Rubén <ruben.crispin@hotmail.com>
2018-10-28 13:02:40 +0100

    Revert "Revert "Merge branch 'master' into camera""
    This reverts commit 2d235e2baaf8e715a9953e9b3fec7cbe317eb689.

---
+++ Engine - Master/Default.fs
@@ -0,0 +1,10 @@
+#version 330 core
+
+out vec4 color;
+
+uniform vec4 newColor;
+
+void main()
+{
+ color = newColor;
+}
--- Engine - Master/ModuleRenderExercise.cpp
+++ Engine - Master/ModuleRenderExercise.cpp
@@ -1,10 +1,10 @@
+<<<<<<< HEAD
 /*#include "Application.h"
 
+=======
+>>>>>>> master
 #include "ModuleRenderExercise.h"
-#include "ModuleWindow.h"
 
-#include "GL/glew.h"
-#include "SDL.h"
 
 ModuleRenderExercise::ModuleRenderExercise()
 {
@@ -16,10 +16,26 @@ ModuleRenderExercise::~ModuleRenderExercise()
 
 bool ModuleRenderExercise::Init()
 {
+<<<<<<< HEAD
 	float3 vertex_buffer_data[] = {
 		{-1.0f, -1.0f, 0.0f},
 		{1.0f, -1.0f, 0.0f},
 		{0.0f,  1.0f, 0.0f}
+=======
+	// Generate program with vertex and fragment shaders and load it to GL
+	program = App->shader->LoadShaders("../default.vs", "../default.fs");
+
+	if (!program) {
+		LOG("Error: Program cannot be compiled");
+		return false;
+	}
+
+	glUseProgram(program);
+	float vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+>>>>>>> master
 	};
 
 	// projection
@@ -104,6 +120,22 @@ update_status ModuleRenderExercise::Update()
 		0,                  // stride
 		(void*)0            // array buffer offset
 	);
+
+	//Use shaders loadeds in program
+	glUseProgram(program);
+
+	// Uniforms (can be changed from any place by calling newColour variable)
+	// Fragment shader coloring
+	int fragUnifLocation = glGetUniformLocation(program, "newColor");
+	float color[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	glUniform4fv(fragUnifLocation, 1, color);
+
+	math::float4x4 Model(math::float4x4::identity); // Not moving anything
+
+	glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_TRUE, &Model[0][0]);
+	//glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_TRUE, &App->camera->LookAt(App->camera->target, App->camera->eye, App->camera->up)[0][0]);
+	//glUniformMatrix4fv(glGetUniformLocation(program, "proj"), 1, GL_TRUE, &App->camera->ProjectionMatrix()[0][0]);
+
 
 	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 	glDisableVertexAttribArray(0);
