efce18c0889395f39a051960f56ba9c8e738f1d6
Rubén <ruben.crispin@hotmail.com>
2018-10-28 12:51:23 +0100

    Ready for using uniforms
    Once added the camera module each vertex can be calculated in shader instead of CPU. Now using uniforms for triangle color.

--- Engine - Master/Default.fs
+++ Engine - Master/Default.fs
@@ -2,7 +2,9 @@
 
 out vec4 color;
 
+uniform vec4 newColor;
+
 void main()
 {
- color = vec4(1.0, 0.0, 0.0, 1.0);
+ color = newColor;
 }
--- Engine - Master/ModuleRenderExercise.cpp
+++ Engine - Master/ModuleRenderExercise.cpp
@@ -50,7 +50,18 @@ update_status ModuleRenderExercise::Update()
 	//Use shaders loadeds in program
 	glUseProgram(program);
 
+	// Uniforms (can be changed from any place by calling newColour variable)
+	// Fragment shader coloring
+	int fragUnifLocation = glGetUniformLocation(program, "newColor");
+	float color[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	glUniform4fv(fragUnifLocation, 1, color);
+
 	math::float4x4 Model(math::float4x4::identity); // Not moving anything
+
+	glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_TRUE, &Model[0][0]);
+	//glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_TRUE, &App->camera->LookAt(App->camera->target, App->camera->eye, App->camera->up)[0][0]);
+	//glUniformMatrix4fv(glGetUniformLocation(program, "proj"), 1, GL_TRUE, &App->camera->ProjectionMatrix()[0][0]);
+
 
 	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 	glDisableVertexAttribArray(0);
