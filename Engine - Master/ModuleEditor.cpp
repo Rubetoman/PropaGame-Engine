6d40bbeb6345f0d5709aec21de376c36064eb2b9
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2018-11-10 17:58:38 +0100

    Revert "Revert "Merge branch 'IMGUI'""
    This reverts commit 08ab23c34de7c7810e61efab7fe6fe64dd5671bb.

--- Engine - Master/ModuleEditor.cpp
+++ Engine - Master/ModuleEditor.cpp
@@ -5,15 +5,17 @@
 #include "ModuleRender.h"
 #include "ModuleTextures.h"
 
+#include "mmgr/mmgr.h"
+
 static void ShowMainMenuBar();
 static void ShowOptionsWindow();
-static void ShowLogWindow();
 static void ShowAboutWindow();
 
 ModuleEditor::ModuleEditor()
 {
 	fps_log.resize(100);
 	ms_log.resize(100);
+	mem_log.resize(100);
 }
 
 // Destructor
@@ -24,7 +26,6 @@ ModuleEditor::~ModuleEditor()
 // Called before render is available
 bool ModuleEditor::Init()
 {
-
 	const char* glsl_version = "#version 130";
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 
@@ -57,7 +58,6 @@ update_status ModuleEditor::PreUpdate()
 	ms_log.erase(ms_log.begin());
 	ms_log.push_back(App->deltaTime * 1000);
 
-
 	return UPDATE_CONTINUE;
 }
 
@@ -73,18 +73,22 @@ update_status ModuleEditor::Update()
 	if (show_about_window) {
 		ShowAboutWindow();
 	}
-	if (show_performance_window) {
-		ShowPerformanceWindow();
+	if (show_app_info_window) {
+		ShowAppInfoWindow();
 	}
 	if (show_textures_window) {
 		//ShowTexturesWindow();
 	}
-	return UPDATE_CONTINUE;
+	if (show_log_window) {
+		ShowLogWindow();
+	}
+	return update;
 }
 
 // Called before quitting
 bool ModuleEditor::CleanUp()
 {
+	Buffer.clear();
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
 	ImGui::DestroyContext();
@@ -103,7 +107,7 @@ static void ShowMainMenuBar()
 		if (ImGui::BeginMenu("File"))
 		{
 			if (ImGui::MenuItem("Open")) {}
-			if (ImGui::MenuItem("Quit", "ALT+F4")) {}
+			if (ImGui::MenuItem("Quit", "ALT+F4")) { App->editor->update = UPDATE_STOP; }
 			ImGui::EndMenu();
 		}
 		if (ImGui::BeginMenu("Edit"))
@@ -118,18 +122,17 @@ static void ShowMainMenuBar()
 		}
 		if (ImGui::BeginMenu("Window"))
 		{
-			if (ImGui::MenuItem("Options")) { App->editor->show_options_window = true; }
-			if (ImGui::MenuItem("Performance")) { App->editor->show_performance_window = true; }
+			if (ImGui::MenuItem("Options", NULL, &App->editor->show_options_window)) { App->editor->show_options_window = true; }
+			if (ImGui::MenuItem("Application Info", NULL, &App->editor->show_app_info_window)) { App->editor->show_app_info_window = true; }
 			if(ImGui::MenuItem("Texture Options")) { App->editor->show_textures_window = true; }
-			if (ImGui::MenuItem("Log")) { App->editor->show_log_window = true; }
-			if (ImGui::MenuItem("Hardware Info")) { ShellExecute(0, 0, "https://github.com/Rubetoman/SDL-OpenGL-Engine-V2", 0, 0, SW_SHOW); }
+			if (ImGui::MenuItem("Log", NULL, &App->editor->show_log_window)) { App->editor->show_log_window = true; }
 			ImGui::EndMenu();
 		}
 		if (ImGui::BeginMenu("Help"))
 		{
-			if (ImGui::MenuItem("About")) { App->editor->show_about_window = true; }
+			if (ImGui::MenuItem("About", NULL, &App->editor->show_about_window)) { App->editor->show_about_window = true; }
 			ImGui::Separator();
-			if (ImGui::MenuItem("View GIT Repository")) { ShellExecute(0, 0, "https://github.com/Rubetoman/SDL-OpenGL-Engine-V2", 0, 0, SW_SHOW); }
+			if (ImGui::MenuItem("View GIT Repository")) { App->editor->ShowInBrowser("https://github.com/Rubetoman/SDL-OpenGL-Engine-V2"); }
 			ImGui::EndMenu();
 		}
 		ImGui::EndMainMenuBar();
@@ -155,9 +158,31 @@ static void ShowOptionsWindow()
 	ImGui::End();
 }
 
-static void ShowLogWindow()
+const void ModuleEditor::ShowLogWindow()
 {
+	ImGui::Begin("Log", &App->editor->show_log_window);   // Pointer to bool variable (close when click on button)
+		ImGui::PushStyleColor(ImGuiCol_Button, { 0.9f,0.45f,0.0f,0.7f });
+		if (ImGui::Button("Clear"))
+		{
+			Buffer.clear();
+		}
+		ImGui::PopStyleColor();
 
+
+	ImGui::TextUnformatted(Buffer.begin());
+
+	if (ScrollToBottom)
+		ImGui::SetScrollHere(1.0f);
+
+	ScrollToBottom = false;
+
+	ImGui::End();
+}
+
+void ModuleEditor::AddLog(const char* logs)
+{
+	Buffer.appendf(logs);
+	ScrollToBottom = true;
 }
 
 static void ShowAboutWindow() 
@@ -165,29 +190,145 @@ static void ShowAboutWindow()
 	ImGui::Begin("About", &App->editor->show_about_window);   // Pointer to bool variable (close when click on button)
 	ImGui::Text(TITLE);
 	ImGui::Text("Version:	BT - 1");
-	ImGui::Text("Games are not free of propaganda");
-	ImGui::Text("C/C++ engine made by Ruben Crispin De la Cruz");
+	ImGui::Text("Games are not free of propaganda.");
+	ImGui::Text("C/C++ engine made by: ");
+	ImGui::NewLine(); ImGui::SameLine(30);
+	if (ImGui::Selectable("Ruben Crispin De la Cruz", false, 0, { 165, 13 }))
+		App->editor->ShowInBrowser("https://github.com/Rubetoman");
+	
+	ImGui::NewLine();
 	ImGui::Text("Libraries: ");
-	ImGui::Text("	- glew-2.1.0");
-	ImGui::Text("	- imgui v1.65 (with branching)");
-	ImGui::Text("	- OpenGL 3.0");
-	ImGui::Text("	- MathGeoLib");
-	ImGui::Text("	- SDL 2.0.8");
+	
+	ImGui::NewLine(); ImGui::SameLine(30);
+	std::string glewVersion = (const char*)glewGetString(GLEW_VERSION);
+	glewVersion = "Glew " + glewVersion;
+	if (ImGui::Selectable(glewVersion.c_str(), false, 0, { 142, 13 }))
+		App->editor->ShowInBrowser("http://glew.sourceforge.net/");
+	
+	ImGui::NewLine(); ImGui::SameLine(30);
+	std::string imguiVersion = "ImGui ";
+	imguiVersion += ImGui::GetVersion();
+	if (ImGui::Selectable(imguiVersion.c_str(), false, 0, { 142, 13 }))
+		App->editor->ShowInBrowser("https://github.com/ocornut/imgui");
+
+	ImGui::NewLine(); ImGui::SameLine(30);
+	std::string glVersion = (const char*)glGetString(GL_VERSION);
+	glVersion = "OpenGL " + glVersion;
+	if (ImGui::Selectable(glVersion.c_str(), false, 0, { 310, 13 }))
+		App->editor->ShowInBrowser("https://www.opengl.org/");
+
+	ImGui::NewLine(); ImGui::SameLine(30);
+	if (ImGui::Selectable("MathGeoLib 1.5.0", false, 0, { 185, 13 }))
+		App->editor->ShowInBrowser("https://github.com/juj/MathGeoLib");
+
+	ImGui::NewLine(); ImGui::SameLine(30);
+	SDL_version version;
+	SDL_GetVersion(&version);
+	std::string sdlVersion = "SDL ";
+	sdlVersion += std::to_string(version.major) + '.' + std::to_string(version.minor) + '.' + std::to_string(version.patch);
+	if (ImGui::Selectable(sdlVersion.c_str(), false, 0, { 135, 13 }))
+		App->editor->ShowInBrowser("https://www.libsdl.org/");
+
+	ImGui::NewLine();ImGui::SameLine(30);
+	std::string assimpVersion = "Assimp - version ";
+	assimpVersion += std::to_string(aiGetVersionMajor()) + '.' + std::to_string(aiGetVersionMinor()) + '.' + std::to_string(aiGetVersionRevision());
+	if (ImGui::Selectable(assimpVersion.c_str(), false, 0, { 155, 13 }))
+		App->editor->ShowInBrowser("http://www.assimp.org/");
+
+	ImGui::NewLine(); ImGui::SameLine(30);
+	std::string devilVersion = "DevIL " + std::to_string(IL_VERSION);
+	if (ImGui::Selectable(devilVersion.c_str(), false, 0, { 135, 13 }))
+		App->editor->ShowInBrowser("http://openil.sourceforge.net/");
+
+	ImGui::NewLine(); ImGui::SameLine(30);
+	if (ImGui::Selectable("mmgr 1.0", false, 0, { 130, 13 }))
+		App->editor->ShowInBrowser("http://www.flipcode.com/archives/Presenting_A_Memory_Manager.shtml");
+
+	// License
+	ImGui::NewLine();
 	ImGui::Text("MIT License Copyright (c) [2018] [Ruben Crispin]");
 
+	// Close button
+	ImGui::NewLine();
 	if (ImGui::Button("Close"))
 		App->editor->show_about_window = false;
 	ImGui::End();
 }
 
-void ModuleEditor::ShowPerformanceWindow()
+void ModuleEditor::ShowAppInfoWindow()
 {
-	ImGui::Begin("Performance", &App->editor->show_performance_window);   // Pointer to bool variable (close when click on button)
-	char title[25];
-	sprintf_s(title, 25, "Framerate %0.1f", fps_log[fps_log.size() - 1]);
-	ImGui::PlotHistogram("##framerate", &fps_log[0], fps_log.size(), 0, title, 0.0f, 200.0f, ImVec2(310, 100));
-	sprintf_s(title, 25, "Milliseconds %0.1f", ms_log[ms_log.size() - 1]);
-	ImGui::PlotHistogram("##framerate", &ms_log[0], ms_log.size(), 0, title, 0.0f, 40.0f, ImVec2(310, 100));
+	ImGui::SetNextWindowSize(ImVec2(350,500), ImGuiCond_FirstUseEver);
+	ImGui::Begin("Application Info", &App->editor->show_app_info_window);   // Pointer to bool variable (close when click on button)
+	if (ImGui::CollapsingHeader("Performance"))
+	{
+		// FPS & Miliseconds for each frame
+		char title[35];
+		sprintf_s(title, 25, "Framerate %0.1f", fps_log[fps_log.size() - 1]);
+		ImGui::PlotHistogram("##framerate", &fps_log[0], fps_log.size(), 0, title, 0.0f, 200.0f, ImVec2(310, 100));
+		sprintf_s(title, 25, "Milliseconds %0.1f", ms_log[ms_log.size() - 1]);
+		ImGui::PlotHistogram("##framerate", &ms_log[0], ms_log.size(), 0, title, 0.0f, 40.0f, ImVec2(310, 100));
+
+		// Memory stats
+		sMStats stats = m_getMemoryStatistics();
+		mem_log.erase(mem_log.begin());
+		mem_log.push_back((float)stats.totalReportedMemory);
+		sprintf_s(title, 35, "Memory Consumption (Bytes) %0.1f", mem_log[mem_log.size() - 1]);
+		ImGui::PlotHistogram("##memory", &mem_log[0], mem_log.size(), 0, title, 0.0f, (float)stats.peakReportedMemory * 1.2f, ImVec2(310, 100));
+		ImGui::Text("Total Reported Mem: %u", stats.totalReportedMemory);
+		ImGui::Text("Total Actual Mem: %u", stats.totalActualMemory);
+		ImGui::Text("Peak Reported Mem: %u", stats.peakReportedMemory);
+		ImGui::Text("Peak Actual Mem: %u", stats.peakActualMemory);
+		ImGui::Text("Accumulated Reported Mem: %u", stats.accumulatedReportedMemory);
+		ImGui::Text("Accumulated Actual Mem: %u", stats.accumulatedActualMemory);
+		ImGui::Text("Accumulated Alloc Unit Count: %u", stats.accumulatedAllocUnitCount);
+		ImGui::Text("Total Alloc Unit Count: %u", stats.totalAllocUnitCount);
+		ImGui::Text("Peak Alloc Unit Count: %u", stats.peakAllocUnitCount);
+	}
+	if (ImGui::CollapsingHeader("Hardware"))
+	{
+		ImGui::Text("CPUs:");
+		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Text, { 1,1,0,1 });
+		ImGui::Text("%d (Cache: %dB)", SDL_GetCPUCount(), SDL_GetCPUCacheLineSize());
+		ImGui::PopStyleColor();
+
+		ImGui::Text("System RAM:");
+		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Text, { 1,1,0,1 });
+		ImGui::Text("%dGb", SDL_GetSystemRAM() / 1000);
+		ImGui::PopStyleColor();
+
+		ImGui::Text("Caps:");
+		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Text, { 1,1,0,1 });
+		std::string caps;
+		if (SDL_Has3DNow())
+			caps += "3DNow!, ";
+		if (SDL_HasAVX())
+			caps += "AVX, ";
+		if (SDL_HasAVX2())
+			caps += "AVX2, ";
+		if (SDL_HasAltiVec())
+			caps += "AltiVec, ";
+		if (SDL_HasMMX())
+			caps += "MMX, ";
+		if (SDL_HasRDTSC())
+			caps += "RDTSC, ";
+		if (caps.size() > 5)
+			caps += "\n";
+		if (SDL_HasSSE())
+			caps += "SSE, ";
+		if (SDL_HasSSE2())
+			caps += "SSE2, ";
+		if (SDL_HasSSE3())
+			caps += "SSE3, ";
+		if (SDL_HasSSE41())
+			caps += "SSE41, ";
+		if (SDL_HasSSE42())
+			caps += "SSE42, ";
+		ImGui::Text(caps.c_str());
+		ImGui::PopStyleColor();
+	}
 	ImGui::End();
 }
 
@@ -266,3 +407,8 @@ void ModuleEditor::ShowPerformanceWindow()
 	}
 	ImGui::End();
 }*/
+
+const void ModuleEditor::ShowInBrowser(const char* url) const
+{
+	ShellExecute(0, "open", url, 0, 0, SW_SHOW);
+}
