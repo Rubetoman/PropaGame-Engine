4b1472a9f9765385fed7d6d706364f845665ce22
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2018-12-14 16:16:36 +0100

    Added material variables

--- Engine - Master/ComponentMaterial.h
+++ Engine - Master/ComponentMaterial.h
@@ -19,13 +19,21 @@ public:
 
 public:
 	unsigned shader = 0;
+	// Diffuse
 	Texture* texture = nullptr;
 	math::float4 color = math::float4::zero;
+	float k_diffuse = 0.5f;
+	// Specular
+	Texture* specular_map = nullptr;
+	math::float3 specular_color = math::float3::zero;
 	float shininess = 64.0f;
 	float k_specular = 1.0f;
-	float k_diffuse = 0.5f;
+	// Ambient
+	Texture* occlusion_map = nullptr;
 	float k_ambient = 0.5f;
-
+	// Emissive
+	Texture* emissive_map = nullptr;
+	math::float3 emissive_color = math::float3::zero;
 };
 
 #endif /*__COMPOMENTMATERIAL_H__*/
