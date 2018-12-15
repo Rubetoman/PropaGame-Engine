4877c5b36e7f108a625f6717f9583e5fcc1801fd
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2018-12-15 13:24:11 +0100

    Improved JSON

--- Engine - Master/JSON.cpp
+++ Engine - Master/JSON.cpp
@@ -2,81 +2,158 @@
 
 #include "Globals.h"
 
-JSON_value::JSON_value(rapidjson::Document::AllocatorType* allocator, rapidjson::Type type) : alloc(allocator)
+#pragma region JSON_value
+
+JSON_value::JSON_value(rapidjson::Document::AllocatorType* allocator) : alloc(allocator)
 {
-	rapidjson_value = new rapidjson::Value(type);
+	value = new rapidjson::Value(rapidjson::kObjectType);
 };
 
-JSON_value* JSON_value::CreateValue(rapidjson::Type type)
+JSON_value::~JSON_value()
 {
-	JSON_value* ret = new JSON_value(alloc, type);
-	values_alloc.push_back(ret);
-	return ret;
+	RELEASE(value);
 }
 
-JSON::JSON()
+JSON_value * JSON_value::createValue()
 {
-	document = new rapidjson::Document();
-	document->SetObject();
-	alloc = &document->GetAllocator();
-	json_buffer = new rapidjson::StringBuffer();
+	return new JSON_value(alloc);
 }
 
-JSON::JSON(const char *data)
+void JSON_value::addValue(const char * name, JSON_value * value)
 {
-	document = new rapidjson::Document();
-	document->SetObject();
-	document->Parse(data);
-	alloc = &document->GetAllocator();
-	json_buffer = new rapidjson::StringBuffer();
-}
-
-JSON::~JSON()
-{
-	RELEASE(document);
-}
-
-
-JSON_value * JSON::CreateValue(rapidjson::Type type)
-{
-	JSON_value* ret = new JSON_value(alloc, type);
-	values_alloc.push_back(ret);
-	return ret;
-}
-
-void JSON::AddValue(const char* name, JSON_value* value)
-{
-	std::string myname(name);
-	rapidjson::Value key(myname.c_str(), myname.size(), *alloc);
-	document->AddMember(key, *value->rapidjson_value, *alloc);
-}
-
-JSON_value * JSON::GetValue(const char* name)
-{
-	if (document->HasMember(name))
+	if (this->value->GetType() == rapidjson::kObjectType)
 	{
-		JSON_value * val = new JSON_value(alloc);
-		val->rapidjson_value->CopyFrom(document->operator[](name), *alloc);
-		values_alloc.push_back(val);
-		return val;
+		std::string str = name;
+		rapidjson::Value index(str.c_str(), str.size(), *alloc);
+		this->value->AddMember(index, *value->getRapidJSONValue(), *alloc);
+	}
+	else if (this->value->GetType() == rapidjson::kArrayType)
+	{
+		this->value->PushBack(*value->getRapidJSONValue(), *alloc);
+	}
+}
+
+JSON_value* JSON_value::getValue(const char * name)
+{
+	if (value->HasMember(name))
+	{
+		rapidjson::Value& trueValue = value->operator[](name);
+		JSON_value* ret = new JSON_value(alloc);
+		ret->getRapidJSONValue()->CopyFrom(trueValue, *alloc, false);
+		return ret;
 	}
 	return nullptr;
 }
 
-
-std::string JSON::ToString()
+void JSON_value::setValue(rapidjson::Value * value)
 {
-	json_buffer->Clear();
-	rapidjson::Writer<rapidjson::StringBuffer> writer(*json_buffer);
-	document->Accept(writer);
-
-	return json_buffer->GetString();
+	RELEASE(this->value);
+	this->value = value;
 }
 
-unsigned JSON::Size()
+rapidjson::Value* JSON_value::getRapidJSONValue()
 {
-	json_buffer->Clear();
-	rapidjson::Writer<rapidjson::StringBuffer> writer(*json_buffer);
-	document->Accept(writer);
-	return json_buffer->GetSize();
+	return value;
 }
+
+#pragma endregion
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#pragma region JSON_file
+
+JSON_file::JSON_file(rapidjson::FileWriteStream* os, FILE* fp) : os(os), fp(fp)
+{
+	document = new rapidjson::Document();
+	document->SetObject();
+	alloc = &document->GetAllocator();
+}
+
+JSON_file::JSON_file(rapidjson::FileReadStream* is, FILE* fp) : is(is), fp(fp)
+{
+	document = new rapidjson::Document();
+	document->ParseStream(*is);
+	alloc = &document->GetAllocator();
+}
+
+JSON_file::~JSON_file()
+{
+	RELEASE(document);
+	RELEASE(is);
+	RELEASE(os);
+}
+
+bool JSON_file::Write()
+{
+	if (os != nullptr)
+	{
+		//rapidjson::Writer<rapidjson::FileWriteStream> writer(*os);
+		rapidjson::PrettyWriter<rapidjson::FileWriteStream> writer(*os);
+		document->Accept(writer);
+		return true;
+	}
+	else
+		return false;
+}
+
+JSON_value* JSON_file::createValue()
+{
+	return new JSON_value(alloc);
+}
+
+void JSON_file::addValue(const char * name, JSON_value* value)
+{
+	std::string str = name;
+	rapidjson::Value index(str.c_str(), str.size(), *alloc);
+	document->AddMember(index, *value->getRapidJSONValue(), *alloc);
+}
+
+JSON_value* JSON_file::getValue(const char* name)
+{
+	if (document->HasMember(name))
+	{
+		rapidjson::Value& value = document->operator[](name);
+		JSON_value* ret = new JSON_value(alloc);
+		ret->getRapidJSONValue()->CopyFrom(value, *alloc, false);
+
+		return ret;
+	}
+	return nullptr;
+}
+
+void JSON_file::closeFile()
+{
+	fclose(fp);
+}
+
+#pragma endregion
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#pragma region JSON
+
+JSON_file* JSON::openReadFile(const char* path)
+{
+	FILE* fp = fopen(path, "rb");
+	char readBuffer[65536];
+
+	return new JSON_file(new rapidjson::FileReadStream(fp, readBuffer, sizeof(readBuffer)), fp);
+}
+
+JSON_file* JSON::openWriteFile(const char * path)
+{
+	FILE* fp = fopen(path, "wb");
+	char writeBuffer[65536];
+
+	return new JSON_file(new rapidjson::FileWriteStream(fp, writeBuffer, sizeof(writeBuffer)), fp);
+}
+
+void JSON::closeFile(JSON_file* file)
+{
+	file->closeFile();
+
+	RELEASE(file);
+	file = nullptr;
+}
+
+#pragma endregion
--- Engine - Master/JSON.h
+++ Engine - Master/JSON.h
@@ -6,6 +6,8 @@
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/filereadstream.h"
 
 #include <string>
 #include <vector>
@@ -14,34 +16,58 @@
 struct JSON_value
 {
 public:
-	JSON_value(rapidjson::Document::AllocatorType* allocator, rapidjson::Type type = rapidjson::kObjectType);
-	JSON_value* CreateValue(rapidjson::Type type = rapidjson::kObjectType);
+	JSON_value(rapidjson::Document::AllocatorType* allocator);
+	~JSON_value();
 
+	JSON_value* createValue();
+	void addValue(const char* name, JSON_value* value);
+	JSON_value* getValue(const char* name);
+
+	void setValue(rapidjson::Value* value);
+	rapidjson::Value* getRapidJSONValue();
 public:
-	rapidjson::Value* rapidjson_value = nullptr;
+	rapidjson::Value* value = nullptr;
 	rapidjson::Document::AllocatorType* alloc = nullptr;
+
 private:
 	std::vector<JSON_value*> values_alloc;
+};
+
+struct JSON_file
+{
+public:
+	JSON_file(rapidjson::FileWriteStream* os, FILE* fp);
+	JSON_file(rapidjson::FileReadStream* is, FILE* fp);
+	~JSON_file();
+
+	bool Write();
+
+	JSON_value* createValue();
+	void addValue(const char* name, JSON_value* value);
+	JSON_value* getValue(const char* name);
+
+	void closeFile();
+
+private:
+
+	FILE* fp = nullptr;
+
+	rapidjson::Document* document = nullptr;
+	rapidjson::FileWriteStream* os = nullptr;
+	rapidjson::FileReadStream* is = nullptr;
+
+	rapidjson::Document::AllocatorType* alloc = nullptr;
 };
 
 class JSON
 {
 public:
-	JSON();
-	JSON(const char* data);
-	~JSON();
+	JSON() {};
+	~JSON() {};
 
-	JSON_value* CreateValue(rapidjson::Type type = rapidjson::kObjectType);
-	void AddValue(const char* name, JSON_value* value);
-	JSON_value* GetValue(const char* name);
-	std::string ToString();
-	unsigned Size();
-
-private:
-	rapidjson::Document* document = nullptr;
-	rapidjson::Document::AllocatorType* alloc = nullptr;
-	rapidjson::StringBuffer* json_buffer = nullptr;
-	std::vector<JSON_value*> values_alloc;
+	JSON_file* openReadFile(const char* path);
+	JSON_file* openWriteFile(const char* path);
+	void closeFile(JSON_file* file);
 };
 
 #endif __JSON_H__
