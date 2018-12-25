6edeaa2839d823efb8a409197b0bbcd8a69e6ab6
Rubén <ruben.crispin@hotmail.com>
2018-12-25 18:31:10 +0100

    Added default textures

--- Engine - Master/ModuleResources.cpp
+++ Engine - Master/ModuleResources.cpp
@@ -1,5 +1,6 @@
 #include "ModuleResources.h"
 
+#include "ModuleTextures.h"
 ModuleResources::ModuleResources()
 {
 }
@@ -7,6 +8,21 @@ ModuleResources::ModuleResources()
 
 ModuleResources::~ModuleResources()
 {
+	App->textures->unloadTexture(checkers_texture);
+	checkers_texture = nullptr;
+	App->textures->unloadTexture(no_camera_texture);
+	no_camera_texture = nullptr;
+}
+
+bool ModuleResources::Init()
+{
+	checkers_texture = App->textures->loadTexture(CHECKERS_TEXTURE);
+	no_camera_texture = App->textures->loadTexture(CAMERA_TEXTURE);
+
+	if (checkers_texture == nullptr || no_camera_texture == nullptr)
+		return false;
+
+	return true;
 }
 
 bool ModuleResources::CleanUp()
