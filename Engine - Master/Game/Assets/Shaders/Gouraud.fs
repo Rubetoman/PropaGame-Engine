5b50f28ae90cd755d2fdd8cb5c6b0e46cb082775
Rubén <ruben.crispin@hotmail.com>
2018-12-31 17:07:51 +0100

    Revert "Updated Gouraud shader [ERROR]"
    This reverts commit 3a6d0c0c488fc6383e6dc6a42e4b7d6f1126a8f2.

--- Engine - Master/Game/Assets/Shaders/Gouraud.fs
+++ Engine - Master/Game/Assets/Shaders/Gouraud.fs
@@ -1,64 +1,25 @@
 #version 330 core
 
-struct Material
-{
-	sampler2D diffuse_map;
-	vec4 diffuse_color;
-	float k_diffuse;
+uniform sampler2D diffuse_map;
+uniform vec4 object_color;
+uniform int use_diffuse_map;
 
-	sampler2D specular_map;
-	vec3 specular_color;
-	float shininess;
-	float k_specular;
-
-	sampler2D occlusion_map;
-	float k_ambient;
-
-    sampler2D emissive_map;
-    vec3      emissive_color;
-};
-
-uniform Material material;
-
-in float specular;
+in float intensity;
 
 in vec2 uv0;
 
 out vec4 color;
 
-vec4 get_diffuse_color()
-{
-	return texture(material.diffuse_map, uv0)*material.diffuse_color;
-}
-
-vec3 get_specular_color()
-{
-	return texture(material.specular_map, uv0).rgb*material.specular_color;
-}
-
-vec3 get_occlusion_color()
-{
-	return texture(material.occlusion_map, uv0).rgb;
-}
-
-vec3 get_emissive_color()
-{
-	return texture(material.emissive_map, uv0).rgb*material.emissive_color;
-}
-
 void main()
 {
+	if(use_diffuse_map == 1)
+    {
+		color = texture2D(diffuse_map, uv0);
+	}
+	else
+	{
+		color = object_color;
+	}
+
     color = vec4(color.rgb*intensity, color.a);
-
-	vec3 emissive_color = get_emissive_color();
-	vec3 occlusion_color= get_occlusion_color();
-	vec3 specular_color = get_specular_color();
-	vec4 diffuse_color = get_diffuse_color();
-    
-	vec3 color_aux = emissive_color + //emissive
-				 diffuse_color.rgb * occlusion_color * material.k_ambient + //ambient
-				 diffuse_color.rgb * diffuse * material.k_diffuse +			//diffuse
-				 specular_color.rgb * specular * material.k_specular;		//specular
-
-    color = vec4(color_aux, diffuse_color.a);
 }
