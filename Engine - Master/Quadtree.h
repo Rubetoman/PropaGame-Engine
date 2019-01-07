ed95c4b96051a99666598a588e0cfd61ace7939f
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-01-07 15:22:40 +0100

    Added Quadtree class

---
+++ Engine - Master/Quadtree.h
@@ -0,0 +1,38 @@
+#ifndef __QUADTREE_H__
+#define __QUADTREE_H__
+
+#include "MathGeoLib.h"
+#include <vector>
+
+class GameObject;
+
+class Quadtree
+{
+public:
+	Quadtree();
+	Quadtree(math::AABB limits, int level);
+	~Quadtree();
+
+	void Insert(GameObject* go);
+	void Intersect(std::vector<GameObject*>& gos, const AABB& bbox);
+
+	void Split();
+
+	void Clear();
+
+	void Draw() const;
+
+public:
+	AABB QuadTree_Box;
+
+	std::vector<GameObject*> container;
+
+	int maxElements = 0;
+	int maxLevels = 0;
+	int level = 0;
+
+	std::vector<Quadtree*> children;
+
+};
+
+#endif // __QUADTREE_H__
